70794fb2501f78c0dfc03c1a7c62d07c1c4689c2
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-22 14:22:48 +0000

    - Coding guidelines applied

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,9 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
 require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
+
 if(TYPO3_MODE == 'FE') tx_div::autoLoadAll($_EXTKEY);
 ?>
